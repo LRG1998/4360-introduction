ca3cb1271a52dfbc47773c38ac5f54093ba05fa6
LRG1998 <ericwarfish212@gmail.com>
2024-08-28 21:20:18 -0500

    Initial commit and wrote index.py

---
+++ index.py
@@ -0,0 +1,38 @@
+import sys
+
+def main():
+    arg = sys.argv[1]
+
+    match arg:
+        case "/name": name()
+
+        case "/fact": fact()
+
+        case "/why": why()
+
+        case "/what": what()
+
+        case _: failsafe()
+
+
+
+def name():
+    print("My name is Jordan Veldkamp")
+
+def why():
+    print("I went into computer science because I wanted to learn to write code to make games. ")
+
+def what():
+    print("After college I hope to get a job releasing either software tools or video games. ")
+
+def fact():
+    print("I use arch linux as my daily driver OS. \n btw.")
+
+def failsafe():
+    print("I have no answer for that!")
+
+
+
+
+if __name__ == "__main__":
+    main()
